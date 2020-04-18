ee1be6bf8767c08e68a4ce6055edff56e364d715
Sarah Matatov <matatov@illinois.edu>
2020-04-18 12:35:22 -0500

    SM_2

---
+++ app/src/main/java/com/example/cs125_finalproject/Constants.java
@@ -0,0 +1,6 @@
+package com.example.cs125_finalproject;
+
+public class Constants {
+    public static int SCREEN_WIDTH;
+    public static int SCREEN_HEIGHT;
+}
---
+++ app/src/main/java/com/example/cs125_finalproject/GameObject.java
@@ -0,0 +1,8 @@
+package com.example.cs125_finalproject;
+
+import android.graphics.Canvas;
+
+public interface GameObject {
+    public void draw (Canvas canvas);
+    public void update();
+}
