f4c3fdd2a341eac07fed5f173d1e1eda218655c4
ellinor <ellinor2@illinois.edu>
2020-04-22 23:30:54 +0200

    Done with part 5 (black screen only music??)

--- app/src/main/java/com/example/cs125_finalproject/GamePanel.java
+++ app/src/main/java/com/example/cs125_finalproject/GamePanel.java
@@ -13,33 +13,19 @@ import android.view.SurfaceView;
 
 public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     private MainThread thread;
-    private RectPlayer player;
-    private Point playerPoint;
-    private ObstacleManager obstacleManager;
-    private boolean movingPlayer = false;
-    private boolean gameOver = false;
-    private long gameOverTime;
-    private Rect r = new Rect();
-    private MediaPlayer mediaPlayer;
+    private SceneManager manager;
 
     public GamePanel(Context context) {
         super(context);
         getHolder().addCallback(this);
         thread = new MainThread(getHolder(), this);
-        player = new RectPlayer(new Rect(100, 100,200, 200),
-                Color.rgb(255, 0,0));
-        playerPoint = new Point(Constants.SCREEN_WIDTH / 2, 3 * Constants.SCREEN_HEIGHT / 4);
-        player.update(playerPoint);
-        obstacleManager = new ObstacleManager(200, 350,75, Color.BLUE, ObstacleManager.highScore);
+
+        manager = new SceneManager();
+
         setFocusable(true);
 
     }
-    public void reset() {
-        playerPoint = new Point(Constants.SCREEN_WIDTH / 2, 3 * Constants.SCREEN_HEIGHT / 4);
-        player.update(playerPoint);
-        obstacleManager = new ObstacleManager(200, 350,75, Color.BLUE, ObstacleManager.highScore);
-        movingPlayer = false;
-    }
+
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) { }
 
@@ -57,83 +43,35 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
             try {
                 thread.setRunning(false);
                 thread.join();
-            } catch(Exception e) {e.printStackTrace();}
+            } catch(Exception e) {e.printStackTrace(); }
             retry = false;
         }
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                if(MainActivity.startGame == 0) {
-                    movingPlayer = true;
-                    MainActivity.startGame++;
-                    break;
-                }
-                if (!gameOver && player.getRectangle().contains((int) event.getX(),
-                        (int) event.getY())) {
-                    movingPlayer = true;
-                    break;
-                }
-                if(gameOver && System.currentTimeMillis() - gameOverTime >= 500) {
-                    reset();
-                    gameOver = false;
-                    MainActivity.startGame = 0;
-                }
-            case MotionEvent.ACTION_MOVE:
-                if (movingPlayer && !gameOver) {
-                    playerPoint.set((int) event.getX(), (int) event.getY());
-                    break;
-                }
-            case MotionEvent.ACTION_UP:
-                movingPlayer = false;
-                break;
-        }
+        manager.recieveTouch(event);
+
         return true;
         //return super.onTouchEvent(event);
     }
 
     public void update(){
-        if (!gameOver) {
-            player.update(playerPoint);
-            if (MainActivity.startGame > 0) {
-                obstacleManager.update();
-            }
-            if (obstacleManager.playerCollide(player)) {
-                gameOver = true;
-                gameOverTime = System.currentTimeMillis();
-            }
-        }
+        manager.update();
     }
 
     @Override
     public void draw(Canvas canvas) {
+        manager.draw(canvas);
+
         super.draw(canvas);
-        canvas.drawColor(Color.WHITE);
-        player.draw(canvas);
-        obstacleManager.draw(canvas);
-        if (gameOver) {
-            Paint paint =  new Paint();
-            paint.setTextSize(100);
-            paint.setColor(Color.BLACK);
-            drawCenterText(canvas, paint, "GAME OVER");
-        }
-        if (MainActivity.startGame == 0) {
+
+        /*if (MainActivity.startGame == 0) {
             Paint paint =  new Paint();
             paint.setTextSize(80);
             paint.setColor(Color.BLACK);
             drawCenterText(canvas, paint, "TAP TO START");
-        }
+        }*/
     }
-    private void drawCenterText(Canvas canvas, Paint paint, String text) {
-        paint.setTextAlign(Paint.Align.LEFT);
-        canvas.getClipBounds(r);
-        int cHeight = r.height();
-        int cWidth = r.width();
-        paint.getTextBounds(text, 0, text.length(), r);
-        float x = cWidth / 2f - r.width() / 2f - r.left;
-        float y = cHeight / 2f - r.height() / 2f - r.bottom;
-        canvas.drawText(text, x, y, paint);
-    }
+
 }
