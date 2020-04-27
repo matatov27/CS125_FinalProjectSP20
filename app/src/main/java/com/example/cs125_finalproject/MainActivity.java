617b307acd6d85d25c620d3864fef32ef768de67
Sarah Matatov <matatov@illinois.edu>
2020-04-27 11:58:13 -0500

    SM_Start Screen Done + Working on Pause Button

--- app/src/main/java/com/example/cs125_finalproject/MainActivity.java
+++ app/src/main/java/com/example/cs125_finalproject/MainActivity.java
@@ -1,21 +1,14 @@
 package com.example.cs125_finalproject;
-import android.app.Activity;
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.DisplayMetrics;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private int START_SCREEN_RUNTIME = 2000;
-    private boolean clicked = false;
     private MotionEvent event;
     private Button startButton;
 
@@ -32,20 +25,6 @@ public class MainActivity extends AppCompatActivity {
                 openGameActivity();
             }
         });
-        /*if (clicked == true) {
-            Intent gameIntent = new Intent(MainActivity.this, GameActivity.class);
-            startActivity(gameIntent);
-            finish();
-        }*/
-        /*new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-
-            }
-        }, START_SCREEN_RUNTIME);*/
-    }
-    public void recieveTouch(MotionEvent event) {
-        clicked = true;
     }
     public void openGameActivity() {
         Intent gameIntent = new Intent(MainActivity.this, GameActivity.class);
