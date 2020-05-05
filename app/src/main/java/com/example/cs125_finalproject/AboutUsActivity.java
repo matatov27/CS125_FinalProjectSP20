7fda97a895b371b3d1f08b586b2190655bac0c83
Sarah Matatov <matatov@illinois.edu>
2020-05-05 08:50:11 -0500

    SM_About Us Screen Added

---
+++ app/src/main/java/com/example/cs125_finalproject/AboutUsActivity.java
@@ -0,0 +1,34 @@
+package com.example.cs125_finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class AboutUsActivity extends AppCompatActivity {
+    private Button backButton;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivity();
+            }
+        });
+    }
+    public void openMainActivity() {
+        Intent aboutUsIntent = new Intent(AboutUsActivity.this, MainActivity.class);
+        startActivity(aboutUsIntent);
+        finish();
+    }
+}
